1e0c5b551dcdef7501286d583ee4089b4eccc3c1
elderhutch <elderhutch@outlook.com>
2025-05-25 12:25:28 -0400

    Logic to repeatedly capture first word is complete

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		words := strings.Fields(scanner.Text())
+		fmt.Println("Your command was:", strings.ToLower(words[0]))
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
