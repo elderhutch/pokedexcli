304306da52d2a979ce53c4224b61fe402729c7e7
elderhutch <elderhutch@outlook.com>
2025-05-25 12:55:55 -0400

    Initial setup for recognizing commands complete

--- main.go
+++ main.go
@@ -7,12 +7,56 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func helpCommand() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Pokedex > ")
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show help",
+			callback:    helpCommand,
+		},
+	}
+
 	for scanner.Scan() {
 		words := strings.Fields(scanner.Text())
-		fmt.Println("Your command was:", strings.ToLower(words[0]))
+		if len(words) == 0 {
+			fmt.Print("Pokedex > ")
+			continue
+		}
+		cmdName := strings.ToLower(words[0])
+		cmd, exists := commands[cmdName]
+		if exists {
+			cmd.callback()
+		} else {
+			fmt.Println("Unknown command:", cmdName)
+		}
+		fmt.Print("Pokedex > ")
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
